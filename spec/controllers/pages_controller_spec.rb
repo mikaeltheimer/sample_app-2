ebf5db42f387155e39861200355e8791f101bfd5
mikael theimer <mikaeltheim@gmail.com>
2013-03-09 12:49:11 -0500

    titres des pages dynamiques avec erb

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,3 +1,6 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe PagesController do
@@ -10,11 +13,23 @@ describe PagesController do
     end
   end
 
+  it "doit avoir le bon titre" do
+    get 'home'
+    response.should have_selector("title",
+                      :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+  end
+
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+  end
+
+  it "doit avoir le bon titre" do
+    get 'contact'
+    response.should have_selector("title",
+                      :content => "Simple App du Tutoriel Ruby on Rails | Contact")
   end
 
     describe "GET 'about'" do
@@ -25,9 +40,9 @@ describe PagesController do
   end
 
   it "doit avoir le bon titre" do
-    get 'home'
+    get 'about'
     response.should have_selector("title",
-                      :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+                      :content => "Simple App du Tutoriel Ruby on Rails | À Propos")
   end
 
 end
