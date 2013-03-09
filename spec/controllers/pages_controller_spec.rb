65e924ef7c633848275b34719dd52751bba88632
mikael theimer <mikaeltheim@gmail.com>
2013-03-09 12:22:55 -0500

    ajouté page about

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -14,6 +15,19 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+  end
+
+    describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  it "doit avoir le bon titre" do
+    get 'home'
+    response.should have_selector("title",
+                      :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
   end
 
 end
